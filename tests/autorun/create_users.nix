93bd4a50af4eca09aaf7d7b2748ffedbe605c60c
Sveske-Juice <carl.benjamin.dreyer@gmail.com>
2025-11-29 22:50:01 +0100

    chore: unit test timeouts

--- tests/autorun/create_users.nix
+++ tests/autorun/create_users.nix
@@ -46,7 +46,7 @@ in {
       # py
       ''
         machine.start()
-        machine.wait_for_unit("jellyfin.service");
+        machine.wait_until_succeeds("test -e /var/lib/jellyfin/init-done", timeout=300)
         machine.wait_until_succeeds("curl 127.0.0.1:8096", timeout=120)
         output = machine.succeed("cat /var/log/jellyfin.txt")
         print("Log: " + output)
